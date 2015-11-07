5e32d2049f940a557db243e25258e4a57423408f
Herman Wahyudi <herman.wahyudi02@gmail.com>
2015-11-07 20:59:39 +0700

    Modified routes users

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+	var j = ['User 1', 'User 2', 'User 3'];
+	var s = "";
+	for(i = 0; i < j.length; i++) {
+  		s += j[i] + "\n";
+  	}
+  	res.send(s);
 });
 
 module.exports = router;
